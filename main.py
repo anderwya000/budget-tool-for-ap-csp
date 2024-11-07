a97f7f74fd9c8856d1b7da83cd70ecaddce92f3e
anderwya000 <anderwya000@students.elkhorn.k12.wi.us>
2024-11-07 06:51:36 -0600

    add elements to listbox. only unique elements are shown?? why?

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import json
 from tkinter import ttk
+from tkinter.ttk import Label
 
 
 def str_to_float(string: str) -> float:
@@ -12,6 +13,7 @@ def str_to_float(string: str) -> float:
 
 
 def main():
+    global data
     with open('data.json', 'r') as file:
         json_data = file.read()
         data = json.loads(json_data)
@@ -21,7 +23,7 @@ def main():
     for i in range(len(data['payments'])):
         total_paid = total_paid + data['payments'][i]['cost']
 
-
+main()
 root = tk.Tk()
 
 # Make main window.
@@ -32,8 +34,17 @@ root.title('Budget Calculator')
 title = tk.Label(root, text='Budget Calculator', font=('helvetica', 22, 'bold'), bg='#FFFFFF', anchor='center')
 title.grid(row=0, column=0, padx=15, pady=15)
 
-items_list = tk.Listbox(root, )
-items_list.grid(row=1, rowspan=5, column=0, padx=15, pady=15)
+items_list = tk.Listbox(root)
+items_list.grid(row=1, rowspan=3, column=0, padx=15, pady=15)
+payments = {}
+for i in range(len(data['payments'])):
+    payments[data['payments'][i]['item']] = data['payments'][i]['cost']
+print(payments)
 
+for i in payments:
+    print(i)
+    tk.Label(items_list, text=i + "    " + str(payments[i]), bg='#FFFFFF').pack(padx=50, pady=2)
+
+items_list.config(height=23, width=35)
 
 root.mainloop()
